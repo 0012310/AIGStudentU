a14829d77f1794fdd9cfbad1e5517d79128945e1
Aman Gangwar <aman.gangwar007@yahoo.com>
2018-04-09 12:21:41 +0530

    09/04/2018 12

---
+++ app/src/main/java/com/aig/advanceinnovationgroup/fragment/ReferFriendFragment.java
@@ -0,0 +1,31 @@
+package com.aig.advanceinnovationgroup.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aig.advanceinnovationgroup.R;
+
+
+public class ReferFriendFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_refer_friend, container, false);
+
+        return view;
+    }
+}
